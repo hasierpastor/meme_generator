ddaedc915898becb300f58fd28026be7eb9f9161
Hasier Pastor <hasier.pastor@gmail.com>
2018-08-19 17:06:07 -0700

    adding files, no styling

---
+++ script.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+  var memeForm = document.getElementById('queryForm');
+  var memeContainer = document.getElementById('meme-container');
+  var memeButton = document.getElementById('queryBtn');
+  var deleteButton = document.getElementsByClassName('delete-button');
+
+  memeButton.addEventListener('click', function(event) {
+    var imageFigure = document.createElement('FIGURE');
+
+    var newMeme = document.createElement('img');
+    newMeme.src = document.getElementById('url').value;
+    newMeme.height = '500';
+    newMeme.width = '500';
+
+    var imageHeader = document.createElement('FIGCAPTION');
+    imageHeader.innerText = document.getElementById('top-text').value;
+
+    var imageFooter = document.createElement('FIGCAPTION');
+    imageFooter.innerText = document.getElementById('bottom-text').value;
+
+    var deleteButton = document.createElement('BUTTON');
+    deleteButton.innerText = 'Delete';
+    deleteButton.type = 'Delete';
+
+    imageFigure.classList.add('image-figure');
+    imageHeader.classList.add('meme-header');
+    imageFooter.classList.add('meme-footer');
+    deleteButton.classList.add('delete-button');
+
+    imageFigure.appendChild(newMeme);
+    imageFigure.appendChild(imageHeader);
+    imageFigure.appendChild(imageFooter);
+    imageFigure.appendChild(deleteButton);
+
+    memeContainer.appendChild(imageFigure);
+
+    deleteButton.addEventListener('click', function(event) {
+      memeContainer.removeChild(imageFigure);
+    });
+
+    memeForm.reset();
+  });
+};
